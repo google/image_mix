f2645a501a15672bd23d6aa80ba3dc436d3156e2
gPS Team <no-reply@google.com>
2023-01-04 18:27:46 -0800

    Internal change
    PiperOrigin-RevId: 499650629
    Change-Id: Ib512558423e4db89884f05c1a3dc5d4cbaa6ca3e

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Google LLC.
+# Copyright 2023 Google LLC.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""setup.py file to make tha package installable with pip install."""
+"""setup.py file to make the package installable with pip install."""
 import setuptools
 
 
